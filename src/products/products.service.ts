dbab7cd91ade00f55506d1a787ecce14bd9ea4f4
Edu <eduardo.lorenzoni@naranja.com>
2024-09-06 19:06:08 -0300

    chore: paginacion

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -17,8 +18,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     return this.product.create({ data: createProductDto });
   }
 
-  findAll() {
-    return this.product.findMany();
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalProducts = await this.product.count();
+    const lastPage = Math.ceil(totalProducts / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit
+      }),
+      meta: {
+        totalProducts,
+        page,
+        lastPage
+      }
+    };
   }
 
   findOne(id: number) {
