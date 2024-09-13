0264d27a969ab14e3d452803ba974fe1d20e7737
Edu <eduardo.lorenzoni@naranja.com>
2024-09-13 11:12:49 -0300

    chore:agrego manejo de excepcion con grpcexception

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -47,7 +48,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: { id, available: true }
     });
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException(`Product with id ${id} not found`);
     }
     return product;
   }
